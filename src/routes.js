c7e19fa59f1eb1cc175827929bba9607d70fe6d1
Joey Nguyen <joey.nguyen@dealertrack.com>
2016-07-16 12:25:43 -0500

    Created new component and route for ManageCoursesPage.

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ import App from './components/App.js';
 import HomePage from './components/home/HomePage.js';
 import AboutPage from './components/about/AboutPage.js';
 import CoursesPage from './components/courses/CoursesPage.js';
+import ManageCoursesPage from './components/courses/ManageCoursesPage.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
     <Route path="courses" component={CoursesPage} />
+    <Route path="course" component={ManageCoursesPage} />
+    <Route path="course/:id" component={ManageCoursesPage} />
   </Route>
 );
