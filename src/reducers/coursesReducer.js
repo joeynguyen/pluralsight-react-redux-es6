251a604357195774b83d466d084de460523008c6
Joey Nguyen <joey.nguyen@dealertrack.com>
2016-07-17 01:54:36 -0500

    Created initialState object and updated references to it.

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -1,6 +1,7 @@
 import * as types from '../actions/actionTypes.js';
+import initialState from './initialState';
 
-export default function coursesReducer(state = [], action) {
+export default function coursesReducer(state = initialState.courses, action) {
   switch(action.type) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
