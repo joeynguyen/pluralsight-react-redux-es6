76a24e69543d724407eb4635fb87ca2d9da7c2b4
Joey Nguyen <joey.nguyen@dealertrack.com>
2016-07-21 00:05:28 -0500

    Added action and reducer to handle AJAX events.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './coursesReducer.js';
 import authors from './authorReducer.js';
+import ajaxStatusReducer from './ajaxStatusReducer.js';
 
 const rootReducer = combineReducers({
   courses: courses,
-  authors: authors
+  authors: authors,
+  ajaxStatusReducer: ajaxStatusReducer
 });
 
 export default rootReducer;
