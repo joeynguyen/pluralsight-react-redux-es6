b7218e4a62896a1e41adb9cdb68c1dc050e513f4
Joey Nguyen <joey.nguyen@dealertrack.com>
2016-07-23 12:03:17 -0500

    Updated app to check ajaxCallsInProgress state to update loading indicator in UI.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { IndexLink, Link } from 'react-router';
 import LoadingDots from './LoadingDots';
 
-const Header = () => {
+const Header = ({loading}) => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -10,9 +10,15 @@ const Header = () => {
       <Link to="/about" activeClassName="active">About</Link>
       {" | "}
       <Link to="/courses" activeClassName="active">Courses</Link>
-      <LoadingDots interval={100} dots={20} />
+      {/*  Uses short-circuiting nature of logical AND operator
+      right hand side will only evaluate if left hand is true  */}
+      {loading && <LoadingDots interval={100} dots={20} />}
     </nav>
   );
 };
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
+};
+
 export default Header;
 
